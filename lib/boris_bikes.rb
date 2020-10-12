91ecf6cba95d54cb218994b24de284d80d32f204
Christian Naykene <younchriss@live.co.uk>
2020-10-12 16:06:40 +0100

    One line test syntax

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,3 +1,7 @@
 class DockingStation
 
+  def release_bike
+
+  end
+
 end
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -3,3 +3,7 @@ require "boris_bikes"
 RSpec.describe "check class exists" do
   docking_station = DockingStation.new
 end
+
+RSpec.describe DockingStation do
+  it { is_expected.to respond_to(:release_bike) }
+end
